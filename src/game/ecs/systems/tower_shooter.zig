0ac4ffd88fc90697aef9f66ebcd8236a1658fcc4
Jacob Enders <jacobenders1@gmail.com>
2026-09-18 23:44:44 -0400

    cleanup

--- src/game/ecs/systems/tower_shooter.zig
+++ src/game/ecs/systems/tower_shooter.zig
@@ -1,30 +1,25 @@
 const zflecs = @import("zflecs");
 const raylib = @import("raylib");
 const components = @import("../components.zig");
-const std = @import("std");
 
-fn system(iter: *zflecs.iter_t) callconv(.c) void {
-    var query_iter = zflecs.query_iter(iter.world, iter.query);
-
-    while (zflecs.query_next(&query_iter) == true) {
-        const position_field = zflecs.field(&query_iter, components.Position, 1) orelse continue;
-        const target_field = zflecs.field(&query_iter, components.Target, 2) orelse continue;
-        for (0..query_iter.count()) |tower_index| {
-            const position = position_field[tower_index];
-            const target = target_field[tower_index];
-            if (target.current_time >= target.max_time) {
-                if (target.position) |target_position| {
-                    const direction = target_position.subtract(position);
-                    createBullet(iter.world, position, .{
-                        .direction = direction,
-                        .speed = 25.0,
-                    });
-                }
-                target_field[tower_index].current_time = 0.0;
-            } else {
-                target_field[tower_index].current_time += raylib.getFrameTime();
-            }
+fn system(iter: *zflecs.iter_t, positions: []components.Position, targets: []components.Target) void {
+    const delta = raylib.getFrameTime();
+    for (0..iter.count()) |index| {
+        targets[index].current_time = targets[index].current_time + delta;
+        if (targets[index].current_time < targets[index].max_time) {
+            continue;
         }
+        const position = positions[index];
+        const target = targets[index];
+        const target_position = target.position orelse continue;
+
+        const direction = target_position.subtract(position);
+        createBullet(iter.world, position, .{
+            .direction = direction,
+            .speed = 25.0,
+        });
+
+        targets[index].current_time = 0.0;
     }
 }
 
@@ -43,29 +38,15 @@ fn createBullet(world: *zflecs.world_t, position: components.Position, direction
 }
 
 pub fn init(world: *zflecs.world_t) void {
-    var query_description: zflecs.query_desc_t = .{};
-    query_description.terms[0] = zflecs.term_t{
-        .id = zflecs.id(components.Tower),
-    };
-    query_description.terms[1] = zflecs.term_t{
-        .id = zflecs.id(components.Position),
-    };
-    query_description.terms[2] = zflecs.term_t{
-        .id = zflecs.id(components.Target),
-    };
-
-    const entity = zflecs.entity_init(world, &zflecs.entity_desc_t{
-        .name = "tower_shooter",
-        .add = &.{
-            zflecs.pair(zflecs.DependsOn, zflecs.OnUpdate),
+    _ = zflecs.ADD_SYSTEM_WITH_FILTERS(
+        world,
+        "tower shooter",
+        zflecs.OnUpdate,
+        system,
+        &.{
+            zflecs.term_t{
+                .id = zflecs.id(components.Tower),
+            },
         },
-    });
-
-    const system_description: zflecs.system_desc_t = .{
-        .callback = system,
-        .entity = entity,
-        .query = query_description,
-    };
-
-    _ = zflecs.system_init(world, &system_description);
+    );
 }
